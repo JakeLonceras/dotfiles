8763307bcef2ea8cf811e197e8e2c23d0c21b99f
reiend <loncerasjakerei@gmail.com>
2023-11-12 02:53:14 +0800

    added autotag

--- config/nvim/lua/user/dneier/plugins/init.lua
+++ config/nvim/lua/user/dneier/plugins/init.lua
@@ -445,5 +445,11 @@ require("lazy").setup({
     config = function()
       require("nvim-autopairs").setup {}
     end
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
   }
 })
