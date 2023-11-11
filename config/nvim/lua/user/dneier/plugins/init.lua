ea1cfaae5bd0ef44a8778eea8478fc0d14afb958
reiend <loncerasjakerei@gmail.com>
2023-11-12 02:37:04 +0800

    added autopairs

--- config/nvim/lua/user/dneier/plugins/init.lua
+++ config/nvim/lua/user/dneier/plugins/init.lua
@@ -440,4 +440,10 @@ require("lazy").setup({
       require('Comment').setup()
     end
   },
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require("nvim-autopairs").setup {}
+    end
+  }
 })
